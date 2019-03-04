1343815e721bcdc46cb4f4e7e7be7ac55fb205e2
hidsh <hideaki.sh@gmail.com>
2019-03-04 12:36:36 +0900

    mod

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 // https://gist.github.com/mitsuruog/fc48397a8e80f051a145
 // 
+
+// sqlite3 test
+var sqlite = require('sqlite3').verbose();
+var db = new sqlite.Database('test.db');
+
 var express = require('express');
 var app = express();
 
@@ -13,5 +18,26 @@ var server = app.listen(3000, function () {
   var host = server.address().address;
   var port = server.address().port;
   console.log('Example app listening at http://localhost:8080 in docker container');
+
+    db.serialize(function() {
+
+        // テーブルがなければ作成
+        // db.run('CREATE TABLE IF NOT EXISTS students(name TEXT, age INT)');
+        
+        // プリペアードステートメントでINSERT
+        var stmt = db.prepare('INSERT INTO orders (name, item) VALUES(?,?)');
+        stmt.run(["ホゲオ", "松"]);
+        stmt.finalize();
+
+        // SELECT
+        db.serialize(function() {
+            db.each("SELECT * FROM orders", function(err, row) {
+                console.log(row);
+            });
+        });
+    });
+
+    db.close();
+
 });
 
